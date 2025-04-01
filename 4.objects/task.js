405f9ffed2bdf16c945eb1ae58f37ae366c9d266
Anastasia Makshukova <mazaeva-94@mail.ru>
2025-04-01 08:43:14 +0300

    выполнено задание объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];   
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+    this.marks.push(...marksToAdd); 
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (this.marks.length === 0) { 
+    return 0;
+  }
+return sum = this.marks.reduce((acc, mark) => acc + mark, 0);
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;  
 }
+
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); 
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); 
+console.log(student1);
