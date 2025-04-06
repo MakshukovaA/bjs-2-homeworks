8dc4d8339720911781e80f995db4497dba852403
Anastasia Makshukova <mazaeva-94@mail.ru>
2025-04-06 14:02:22 +0300

    Задание объекты2

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -10,14 +10,17 @@ Student.prototype.setSubject = function (subjectName) {
 }
 
 Student.prototype.addMarks = function (...marksToAdd) {
+  if (this.marks) { 
     this.marks.push(...marksToAdd); 
+} 
 }
 
 Student.prototype.getAverage = function () {
-  if (this.marks.length === 0) { 
+  if (!this.marks || this.marks.length === 0) { 
     return 0;
   }
-return sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+  const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+  return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
@@ -30,5 +33,7 @@ let student1 = new Student("Василиса", "женский", 19);
 student1.setSubject("Algebra");
 console.log(student1.getAverage()); 
 student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); 
+student1.exclude("Непосещение занятий");
 console.log(student1.getAverage()); 
 console.log(student1);
